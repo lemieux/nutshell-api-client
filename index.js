8986f6129de0e6b90311b1547ad03e3c11b84f31
Matthew Dickinson <matt@codifi.us>
2016-10-04 13:33:25 -0400

    Updated code to use a constructor to be more efficient.

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const api = require('./lib/api-client');
 // Public Interface
 //------------------------------------------------------------------------------
 
-exports.find = function (entity, args, cb) {
+const NutshellApiClient = function(args) {
+  const self = this;
   let options = {
     method   : 'POST',
     path     : '/api/v1/json',
@@ -18,11 +19,17 @@ exports.find = function (entity, args, cb) {
     password : args.password
   };
 
-  api.newNutshell(options, function (err, nutshell) {
-    if (err) {
-      console.error(err);
-      return;
-    }
+  this.nutshellPromise = new Promise((resolve, reject) => {
+    api.newNutshell(options, function (err, nutshell) {
+      if (err) {
+        return reject(err);
+      }
+      resolve(nutshell);
+    });
+  })
+
+
+  this.find = function (entity, args, cb) {
     let findArgs = { entity };
     if (args.qty) {
       findArgs.qty = args.qty;
@@ -39,12 +46,20 @@ exports.find = function (entity, args, cb) {
     if (args.query) {
       findArgs.query = args.query;
     }
-    nutshell.find(findArgs, function (err, results) {
+    self.nutshellPromise.then(nutshell => nutshell.find(findArgs, function (err, results) {
       if (err) {
         console.error(err);
         return cb(err);
       }
       cb(null, results);
-    });
-  });
+    }));
+  };
+
+  this.get = function(entity, entityId, cb) {
+    self.nutshellPromise.then(nutshell => {
+      nutshell.get(entity, entityId, cb);
+    }).catch(err => console.log('nutshell unavailable', err));
+  };
 };
+
+module.exports = NutshellApiClient;
